3c2955fe75fbf04bff25f3d3889a8001cc555393
gitrectify <gitrectify@gmail.com>
2026-09-21 08:32:04 +0900

    perf: update AppLovinSDK

--- Assets/MaxSdk/Scripts/MaxSdk.cs
+++ Assets/MaxSdk/Scripts/MaxSdk.cs
@@ -14,7 +14,7 @@ public class MaxSdk :
     MaxSdkUnityEditor
 #endif
 {
-    private const string _version = "8.6.5";
+    private const string _version = "8.6.6";
 
     /// <summary>
     /// Returns the current plugin version.
